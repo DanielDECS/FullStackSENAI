85d81c93f8d361e48945f8c965ed9acc202eedd9
Daniel <dancarsoa@gmail.com>
2022-01-26 17:12:31 -0300

    Alteração na lista de participantes

--- eventos.js
+++ eventos.js
@@ -9,18 +9,26 @@ if (nomeEvento != "") {
     let data = new Date(partesData[2], partesData[1] - 1, partesData[0]);
     if (data > new Date()) {
         alert(`A data: ${dataEvento} para o evento é válida, seguindo com o cadastro...`);
-
         alert("Cadastro de palestrantes!");
         let nomePalestrantes = prompt("Escreva o nome dos palestrantes separados por vírgula:")
 
-        alert("Verificação inicial para cadastro de participante!");
-        let idadeParticipante = parseInt(prompt("Entre com a idade do participante"))
-        if (idadeParticipante >= 18) {
-            let nomeParticipante = prompt("Escreva o nome do participante")
-            alert(`A lista com o nome dos palestrantes é: ${nomePalestrantes}.`)
-            alert(`O participante cadastrado é: ${nomeParticipante}.`)
+        let listaParticipantes = ["Danilo", "Marcelo", "Alan"];
+        if (listaParticipantes.length <= 100) {
+            alert("Verificação inicial para cadastro de participante!");
+            let idadeParticipante = parseInt(prompt("Entre com a idade do participante"))
+            if (idadeParticipante >= 18) {
+                let novoParticipante = prompt("Escreva o nome do participante")
+                listaParticipantes.push = novoParticipante;
+            } else {
+                alert("Não são permitidos menores de 18 anos no evento!");
+            }
         } else {
-            alert("Não são permitidos menores de 18 anos no evento!");
+            alert("O total de participantes excedeu o limite permitido");
+        }
+        alert(`A lista com o nome dos palestrantes é: ${nomePalestrantes}.`)
+        alert(`Os participantes cadastrados são:`);
+        for(let i = 0; i < listaParticipantes.length; i++) {
+           alert(listaParticipantes[i]); 
         }
     } else {
         alert("A data do evento não é válida!");
