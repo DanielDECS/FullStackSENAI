c90c5406f82e971e53bc8d344e6b08c1c7fcaed3
Daniel <dancarsoa@gmail.com>
2022-06-27 18:57:34 -0400

    Adicionada as funções JS de toogle, carrousel e registro

--- js/script.js
+++ js/script.js
@@ -1,35 +1,41 @@
 
-// Função de exibição do menu de barras para layout responsivo
-// $(document).ready(function() {
-//         // Toggle manual
-//         if($("#menu").hasClass("menu-active")) {
-//             $("#menu").removeClass("menu-active");
-//         } else {
-//             $("#menu").addClass("menu-active");
-//         }
-
-// })
-
-// setInterval(changeProduct, 1000);
-
-function changeProduct() {
+//Função de exibição do menu carrousel
+function carrousel() {
     let product1 = document.getElementById("product1");
-    getComputedStyle(product1).visibility = "hidden";
+    let product2 = document.getElementById("product2");
+    let product3 = document.getElementById("product3");
+    let product4 = document.getElementById("product4");
+    let slideAtual = 1;
+    let listaSlides = [product1, product2, product3, product4];
+
+    setInterval(changeProduct, 2000);
+
+    function changeProduct() {
+        console.log("Slide atual: " + slideAtual);
+        // Remove o slide anterior
+        if (slideAtual > 0) {
+            listaSlides[slideAtual - 1].style.display = "none";
+        } else if (slideAtual == 0) {
+            listaSlides[(listaSlides.length) - 1].style.display = "none";
+        }
+
+        // Adiciona o  slide atual
+        (listaSlides[slideAtual]).style.display = "flex";
+
+        // Indica o side atual
+        slideAtual++
+
+        if(slideAtual > (listaSlides.length) - 1){
+            slideAtual = 0
+        }
+    }
 }
 
-//     if(getComputedStyle(product1).display == "none" && 
-//     getComputedStyle(product2).display == "none") {
-//     product1.style.display = "flex";
-//     product2.style.display = "flex";
-//     product3.style.display = "none";
-//     product4.style.display = "none";
-// } 
-    
-
+//Função de exibição do menu de barras para layout responsivo
 function showMenu() {
     let menu = document.getElementById("menu");
 
-    if(getComputedStyle(menu).display == "none") {
+    if (getComputedStyle(menu).display == "none") {
         menu.style.display = "flex";
     } else {
         menu.style.display = "none"
