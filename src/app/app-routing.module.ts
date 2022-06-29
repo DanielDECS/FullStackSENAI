64a1f5d5371b6de134c9bdacc3c9c885e756f613
Daniel <dancarsoa@gmail.com>
2022-06-28 23:17:33 -0400

    Criado o componente carousel

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './views/home/home.component';
 import { LoginComponent } from './views/login/login.component';
+import { CarouselComponent } from './views/carousel/carousel.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'login', component: LoginComponent }
+  { path: 'login', component: LoginComponent },
+  { path: 'carousel', component: CarouselComponent }
+
 ];
 
 @NgModule({
--- src/app/views/carousel/carousel.component.ts
+++ src/app/views/carousel/carousel.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-carousel',
   templateUrl: './carousel.component.html',
-  styleUrls: ['./carousel.component.css']
+  styleUrls: ['./carousel.component.css'],
+  providers: [NgbCarouselConfig]  // add NgbCarouselConfig to the component providers
 })
-export class CarouselComponent implements OnInit {
 
-  constructor() { }
+export class CarouselComponent {
 
-  ngOnInit(): void {
+  constructor(config: NgbCarouselConfig) {
+    // customize default values of carousels used by this component tree
+    config.interval = 10000;
+    config.wrap = false;
+    config.keyboard = false;
+    config.pauseOnHover = false;
   }
-
 }
+
+
+
