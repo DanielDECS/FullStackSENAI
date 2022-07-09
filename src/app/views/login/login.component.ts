13afcbe51c38e1b47d0275f29fa04bdad5ccdf67
Daniel <dancarsoa@gmail.com>
2022-07-08 20:12:14 -0400

    Criada a estrutura de tratamento de erros na função de login

--- src/app/views/login/login.component.ts
+++ src/app/views/login/login.component.ts
@@ -15,6 +15,7 @@ export class LoginComponent implements OnInit {
   }
 
   userModel = new User()
+  message = ""
 
   receiveData() {
     console.log(this.userModel)
@@ -22,8 +23,17 @@ export class LoginComponent implements OnInit {
     // Enviar dados para a API
     this.loginService.login(this.userModel).subscribe( (response) => {
       console.log("response: ", response);
+      console.log("O status code é: ", response.status);
+      console.log("O token de permissão é: ", response.body.acessToken);
+
+      this.message = "Bem vindo " + response.body.user.nome + "!"
+      console.log(this.message);
+
     }, (error) => {
       console.log("error: ", error);
+      this.message = "Ocorreu um erro, tente novamente! " + " ( " + error.error + " ) ";
+      // this.message = "Ocorreu um erro, tente novamente ! " + error.error;
+
     })
   }
 
