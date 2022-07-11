3678f9bab249c4d734d9b82b1fc9dcc327b3aa81
Daniel <dancarsoa@gmail.com>
2022-07-11 13:20:25 -0400

    Configurado comandos contra ataques do SQL Injection para proteção do formulário de login

--- src/app/views/login/login.component.ts
+++ src/app/views/login/login.component.ts
@@ -8,8 +8,9 @@ import { LoginService } from 'src/app/services/login.service';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  router: any;
 
-  constructor( private loginService: LoginService ) { }
+  constructor(private loginService: LoginService) { }
 
   ngOnInit(): void {
   }
@@ -18,23 +19,32 @@ export class LoginComponent implements OnInit {
   message = ""
 
   receiveData() {
-    console.log(this.userModel)
+    console.log("Modelo: ", this.userModel)
+
+    // Verificação de segurança para prevenir SQL Injection
+    const wordsList: string[] = ["select ", "from ", "drop ", "or ", "having ", "group ", "by ", "insert ", "exec ", "\"", "\'", "--", "#", "*", ";"]
+
+    wordsList.forEach(word => {
+      if(this.userModel.email?.toLowerCase().includes(word)) {
+        this.message = "Dados inválidos " + "( " + word + " )"
+        return;
+      }
+    })
 
     // Enviar dados para a API
-    this.loginService.login(this.userModel).subscribe( (response) => {
+    this.loginService.login(this.userModel).subscribe((response) => {
       console.log("response: ", response);
       console.log("O status code é: ", response.status);
       console.log("O token de permissão é: ", response.body.acessToken);
 
       this.message = "Bem vindo " + response.body.user.nome + "!"
       console.log(this.message);
+      this.router.navigateByUrl('/')
 
     }, (error) => {
       console.log("error: ", error);
       this.message = "Ocorreu um erro, tente novamente! " + " ( " + error.error + " ) ";
-      // this.message = "Ocorreu um erro, tente novamente ! " + error.error;
 
     })
   }
-
 }
