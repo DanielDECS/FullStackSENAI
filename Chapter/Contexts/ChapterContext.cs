9a0b8dee2dffe3b3690edf051d55b6d748277284
Daniel <dancarsoa@gmail.com>
2022-10-21 22:06:29 -0400

    Realizada a atividade do encontro remoto 3

--- Chapter/Contexts/ChapterContext.cs
+++ Chapter/Contexts/ChapterContext.cs
@@ -11,18 +11,21 @@ namespace Chapter.Contexts
         public ChapterContext(DbContextOptions<ChapterContext> options) : base(options)
         {
         }
-
-        //Método de configuração de banco de dados
+        //vamos utilizar esse método para configurar o banco de dados
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             if (!optionsBuilder.IsConfigured)
             {
+                //cada provedor tem sua sintaxe para especificação
                 optionsBuilder.UseSqlServer("Data Source = DANIEL\\SQLEXPRESS; initial catalog = Chapter; Integrated Security = true");
             }
         }
 
-        //O dbset representa as entidades utilizadas nas operações de leitura, criação, atualização e exclusão.
+        //dbset representa as entidades que serão utilizadas nas operações de leitura, criação, atualização e exclusão.
         public DbSet<Livro> Livros { get; set; }
+        public DbSet<Usuario> Usuarios { get; set; }
 
     }
 }
+
+
