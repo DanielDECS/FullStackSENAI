91b5f2ba1a98c45dfab64b427724be34110e6a81
Daniel <dancarsoa@gmail.com>
2022-10-21 23:23:12 -0400

    incluido o arquivo de documentação da ExoAPI

--- ExoApi/Program.cs
+++ ExoApi/Program.cs
@@ -1,8 +1,49 @@
+using ExoApi.Contexts;
+using ExoApi.Interfaces;
+using ExoApi.Repositories;
+using Microsoft.IdentityModel.Tokens;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddScoped<dbExoAPIContext, dbExoAPIContext>();
+builder.Services.AddScoped<ProjetoRepository, ProjetoRepository>();
+
+builder.Services.AddTransient<IUsuarioRepository, UsuarioRepository>();
+
 builder.Services.AddControllers();
+
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("CorsPolicy", builder =>
+    {
+        builder.WithOrigins("http://localhost:3000")
+        .AllowAnyHeader()
+        .AllowAnyMethod();
+    });
+
+});
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultChallengeScheme = "JwtBearer";
+    options.DefaultAuthenticateScheme = "JwtBearer";
+}).AddJwtBearer("JwtBearer", options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        IssuerSigningKey = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes("exo-chave-autenticacao")),
+        ClockSkew = TimeSpan.FromMinutes(60),
+        ValidIssuer = "exo.webapi",
+        ValidAudience = "exo.webapi"
+    };
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -17,6 +58,10 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors("CorsPolicy");
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
